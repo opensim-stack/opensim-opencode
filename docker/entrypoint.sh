74718e8ef97c7318bc90c7b2e91c1cb7c199b104
Brett Smith <tanktarta@gmail.com>
2026-08-23 17:51:41 +0100

    Always `serve`

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,6 @@ set -eu
 OPENCODE_HOST="${OPENCODE_HOST:-0.0.0.0}"
 OPENCODE_PORT="${OPENCODE_PORT:-8998}"
 OPENCODE_PROJECT_DIR="${OPENCODE_PROJECT_DIR:-/workspace}"
-OPENCODE_MODE="${OPENCODE_MODE:-serve}"
 OPENCODE_SERVER_EXTRA_ARGS="${OPENCODE_SERVER_EXTRA_ARGS:-}"
 
 mkdir -p \
@@ -21,4 +20,4 @@ if [ "$#" -gt 0 ]; then
 fi
 
 # shellcheck disable=SC2086
-exec opencode "${OPENCODE_MODE}" --hostname "${OPENCODE_HOST}" --port "${OPENCODE_PORT}"  ${OPENCODE_SERVER_EXTRA_ARGS}
+exec opencode serve --hostname "${OPENCODE_HOST}" --port "${OPENCODE_PORT}"  ${OPENCODE_SERVER_EXTRA_ARGS}
